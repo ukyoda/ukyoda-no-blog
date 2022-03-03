1a652230549e0eb87a6b99c1e45f0e9f4751c0c4
ukyoda <black12172001@hotmail.co.jp>
2022-03-03 22:29:08 +0900

    fix compiler options (#2)
    * fix type
    
    * upgrade types
    
    * fix plugin
    
    * generate dts files
    
    * add gatsby command for npm scripts
    
    * fix eslint
    
    * fix lint but occured build warning orz
    
    * fix
    
    * fix
    
    * fix gatsby-types

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,7 +14,20 @@ const {
   onCreateNode,
   createSchemaCustomization,
 } = require('./src/gatsby-node/index')
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin')
 
 exports.createPages = createPages
 exports.onCreateNode = onCreateNode
 exports.createSchemaCustomization = createSchemaCustomization
+
+//robertmarshall.dev/blog/fix-warn-chunk-commons-mini-css-extract-plugin-error-in-gatsby-js/
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new FilterWarningsPlugin({
+        exclude:
+          /mini-css-extract-plugin[^]*Conflicting order. Following module has been added:/,
+      }),
+    ],
+  })
+}
