b7d7c050a3944d65d6221463c7429881335ac4ac
ukyoda <black12172001@hotmail.co.jp>
2022-04-02 21:32:54 +0900

    fix

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'prismjs/themes/prism-okaidia.css'
 import { RecoilRoot } from 'recoil'
-import { ModalContainer } from './src/components/Modal/useModal'
+import { ModalContainer } from './src/components/modals/Modal/useModal'
 
 import './src/styles/global.css'
 
