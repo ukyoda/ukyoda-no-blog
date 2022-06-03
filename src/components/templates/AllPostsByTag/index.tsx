6dccceb84e0496976c05d27c97dae1c27e96291e
ukyoda <black12172001@hotmail.co.jp>
2022-06-04 02:49:01 +0900

    implement all posts by tag page

--- src/components/templates/AllPostsByTag/index.tsx
+++ src/components/templates/AllPostsByTag/index.tsx
@@ -1,5 +1,7 @@
 import React, { ComponentProps } from 'react'
 
+import * as styles from './AllPostsByTag.module.css'
+
 import { CardList } from '~/components/molecules/CardList'
 
 type PostProps = ComponentProps<typeof CardList>['items'][number]
@@ -11,13 +13,13 @@ type Props = {
 
 export const AllPostsByTag: React.FC<Props> = ({ posts, tagName }) => {
   return (
-    <main>
-      <header>
-        <h1>「{tagName}」のブログ記事一覧</h1>
+    <article className={styles.article}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>「{tagName}」のブログ記事一覧</h1>
       </header>
       <section>
         <CardList items={posts} keyPrefix="SearchResultList" />
       </section>
-    </main>
+    </article>
   )
 }
